2152005e13a90ae2de9d502c127f9f1565ac5f2c
ayyash <abdul_ayyash@yahoo.com>
2025-03-04 22:18:47 +0300

    connect to render

---
+++ client/chunk-6OUJ5KKQ.js
@@ -0,0 +1,1 @@
+var e=class{static NewInstance(a){return{displayname:a.displayname,avatar:a.avatar,username:a.shortname}}};export{e as a};
--- client/chunk-K7TQ6VIG.js
+++
@@ -1,1 +0,0 @@
-import{i as n}from"./chunk-MXNVAF7S.js";var t=class{static MapLanguage(a){return a?a[n.language]:""}static PrepLanguage(a){return{en:a.en,ar:a.ar,ku:a.ku}}};export{t as a};
